ee681cb6c6adefa4e34c67d6bd594a493595dc45
Martin <martinzelikovsky@gmail.com>
2026-02-13 23:28:11 -0800

    Implemented first pass at main loop.

--- deal_analyzer.py
+++ deal_analyzer.py
@@ -50,16 +50,42 @@ class DealAnalyzer:
             self.current_csv = self.get_current_csv()
             self.current_df = self.get_current_df()
             self.current_asin = self.get_asin_checkpoint()
-        else:
-            # create result file
-            self.create_result_file()
-        
-        
+        # else:
+        #     # create result file
+        #     self.create_result_file()
+
         # iterate over the current and remaining tabs
         # for each tab sort the asins and continue from the current asin
+        input_file_list = self.input_files()[self.input_files.index(self.checkpoint_file):]
+
+        for file in input_file_list:
+            excel = pd.ExcelFile(file)
+            tab_list = list(filter(lambda x: re.match(self.tab_regex, x), excel.sheet_names))
+            if self.current_tab:
+                remaining_tabs = tab_list[tab_list.index(self.current_tab):]
+            else: 
+                self.current_tab = tab_list[0]
+                remaining_tabs = tab_list
+            for tab in remaining_tabs:
+                sheet_df = excel.parse(tab).sort_values('B00 ASIN')  # TODO: specify dtype
+                for i, asin in enumerate(sheet_df['B00 ASIN']):
+                    keepa_df = self.keepa_client.get_asin_df(asin)  # TODO: continue after implementing keepa client
+                    self.current_df = sheet_df.merge(keepa_df, how='left', left_on=['B00 ASIN'], right_on=['asin'])
+                    if i % 10 == 0:
+                        self.current_df.to_csv(self.current_csv)  # Checkpoint save
+                self.current_df.to_csv(self.current_csv)  # Checkpoint save
+                # TODO: Save CSV into an Excel book
+                writer = pd.ExcelWriter(self.output_file, engine='xlsxwriter')  
+                self.current_df.to_excel(writer, sheet_name=self.current_tab, index=False)
+
+
     
     def get_current_df(self) -> pd.DataFrame:
-        df = pd.read_csv(self.current_csv)  # TODO: specify dtype map for correct parsing
+        df = None
+        if os.path.exists(self.current_csv):
+            df = pd.read_csv(self.current_csv)  # TODO: specify dtype map for correct parsing
+
+        return df
 
 
     def get_current_csv(self) -> str:
@@ -70,19 +96,21 @@ class DealAnalyzer:
         return f'{self.RESULT_TAB_REGEX.replace('\d+', f"{tab_num}")}'
 
     def get_tab_checkpoint(self) -> str:
-        max_tab = None
+        tab_checkpoint = None
         output_file = pd.ExcelFile(self.output_file)
         result_sheets = filter(lambda x: re.match(self.RESULT_TAB_REGEX, x), output_file.sheet_names)
         if result_sheets:
             max_tab = max(result_sheets, key=lambda x: int(re.search('\d+', x).group()))
-            tab_checkpoint = self.get_result_tab_name(max_tab + 1)
+            tab_checkpoint = self.get_result_tab_name(max_tab + 1)  # TODO: add check to confirm tabs are done
 
         return tab_checkpoint
     
     def get_asin_checkpoint(self) -> str:
         # read file and find the ASIN at the last line
-        pass
-        
+        asin_checkpoint = None
+        if self.current_df is not None:
+            asin_checkpoint = self.current_df['asin'][-1]  # TODO: verify
+        logger.info(f'ASIN checkpoint {asin_checkpoint=}')
 
     def get_max_asin_from_tab(self, tab_name: str) -> str:
         max_asin = None
