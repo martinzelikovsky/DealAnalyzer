70ed568aa912ce31b31beaaffd274794789f0c2a
Martin <martinzelikovsky@gmail.com>
2026-02-24 22:57:40 -0800

    Fixed logging!

--- src/utils.py
+++ src/utils.py
@@ -7,10 +7,11 @@ def config_logger(output_dir: str, filename: str, logger: logging.Logger):
     log_file = Path(output_dir) / filename
     format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s')
     fh = logging.FileHandler(log_file)
-    fh.setLevel = logging.DEBUG
+    fh.setLevel(logging.DEBUG)
     fh.setFormatter(format)
     sh = logging.StreamHandler(sys.stdout)
-    sh.setLevel = logging.INFO
+    sh.setLevel(logging.INFO)
     sh.setFormatter(format)
+    logger.setLevel(logging.DEBUG)
     logger.addHandler(fh)
     logger.addHandler(sh)
